e1d8bb2883fc66faeded16b84e0745747bb53fd1
Zubenzitkar <zubenzitkar@gmail.com>
2023-08-15 16:41:44 +0300

    0.02.0

--- presentation/src/main/java/com/example/presentation/screen/StartScreen.kt
+++ presentation/src/main/java/com/example/presentation/screen/StartScreen.kt
@@ -2,21 +2,12 @@ package com.example.presentation.screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.presentation.R
@@ -25,32 +16,23 @@ import com.example.presentation.R
 @Composable
 fun StartScreen() {
 
-    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.start_screen_animation))
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.start_screen_animation)
+    )
 
-    val isPlaying by remember {
-        mutableStateOf(true)
-    }
-    val progress by animateLottieCompositionAsState(composition = composition, isPlaying = isPlaying)
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+
 
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        Box(
-            modifier = Modifier
-                .align(Alignment.Center)
-        ) {
-            /*Text(
-                text = "MuPlayer",
-                fontFamily = FontFamily(Font(R.font.irish_grover_regular)),
-                fontSize = 40.sp
-            )*/
-            LottieAnimation(
-                modifier = Modifier.fillMaxSize(),
-                composition = composition,
-                progress = { progress }
-            )
-
-
-        }
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
