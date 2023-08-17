ebf98b4e917227458436592f852935003906e7bd
Zubenzitkar <zubenzitkar@gmail.com>
2023-08-17 15:53:46 +0300

    0.02.2

--- presentation/src/main/java/com/example/presentation/panel/BottomPanel.kt
+++ presentation/src/main/java/com/example/presentation/panel/BottomPanel.kt
@@ -1,115 +1,90 @@
 package com.example.presentation.panel
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.material3.Icon
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.presentation.R
 
 
+@Preview(showBackground = true)
 @Composable
 fun BottomPanel() {
-
-    BottomAppBar (
-        containerColor = Color(0xFFFBF7F7),
-        contentColor = Color(0xFF797373)
-    ) {
-        IconBottomPanel(
-            icon = painterResource(id = R.drawable.queue_music),
-            contentDescription = "Playlist",
-            text = "Playlist"
-        )
-
-        IconBottomPanel(
-            icon = painterResource(id = R.drawable.queue_music),
-            contentDescription = "Playlist",
-            text = "Playlist"
-        )
-
-        IconBottomPanel(
-            icon = painterResource(id = R.drawable.queue_music),
-            contentDescription = "Playlist",
-            text = "Playlist"
-        )
-
-    }
-
-    /*BottomAppBar(
+    BottomAppBar(
         containerColor = Color(0xFFFBF7F7),
         contentColor = Color(0xFF797373)
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly,
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            IconButton(
-                modifier = Modifier.fillMaxWidth(0.2f),
-                onClick = {  }
-            ) {
-                Text(
-                    text = "Setting",
-                    fontSize = 18.sp
-                )
-            }
-            IconButton(
-                modifier = Modifier.fillMaxWidth(0.2f),
-                onClick = {  }
-            ) {
-                Text(
-                    text = "Home",
-                    fontSize = 18.sp
-                )
-            }
-            IconButton(
-                modifier = Modifier.fillMaxWidth(0.3f),
-                onClick = {  }
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.queue_music),
-                    contentDescription = "Playlist"
-                )
+            IconBottomPanel(
+                icon = painterResource(id = R.drawable.queue_music),
+                contentDescription = "Music",
+                text = "Music",
+                alignment = Alignment.Start
+            )
 
-               
-            }
+            IconBottomPanel(
+                icon = painterResource(id = R.drawable.playlist_music),
+                contentDescription = "Playlist",
+                text = "Playlist",
+                alignment = Alignment.CenterHorizontally
+            )
+
+            IconBottomPanel(
+                icon = painterResource(id = R.drawable.setting_default),
+                contentDescription = "Setting",
+                text = "Setting",
+                alignment = Alignment.End
+            )
         }
-    }*/
+    }
 }
 
-
 @Composable
-fun IconBottomPanel(icon: Painter, contentDescription: String, text: String) {
+fun IconBottomPanel(
+    icon: Painter,
+    contentDescription: String,
+    text: String,
+    alignment: Alignment.Horizontal,
+) {
+    Column(
+        modifier = Modifier.clickable {
 
-    IconButton(
-        modifier = Modifier
-            .fillMaxWidth(0.3f)
-            .fillMaxHeight(),
-        onClick = { }
+        },
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Icon(
-                painter = icon,
-                contentDescription = contentDescription
-            )
-            Text(text = text)
-        }
+        Icon(
+            painter = icon,
+            contentDescription = contentDescription,
+            modifier = Modifier.size(30.dp)
+        )
 
+        Text(
+            text = text,
+            modifier = Modifier.align(alignment)
+        )
     }
-
 }
 
 
