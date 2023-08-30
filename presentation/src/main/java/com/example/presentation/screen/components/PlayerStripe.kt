5c08775552b714e928f0a9f27f00a46ad86439b4
Zubenzitkar <zubenzitkar@gmail.com>
2023-08-30 18:01:59 +0300

    0.03.21

--- presentation/src/main/java/com/example/presentation/screen/components/PlayerStripe.kt
+++ presentation/src/main/java/com/example/presentation/screen/components/PlayerStripe.kt
@@ -11,15 +11,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -37,6 +40,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.example.presentation.R
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
@@ -44,24 +48,39 @@ import kotlin.math.roundToInt
 @Composable
 fun PlayerStripe() {
 
+    val time = 10000
 
+    var progress by remember { mutableFloatStateOf(0f) }
     var offsetX by remember { mutableFloatStateOf(0f) }
     val scope = rememberCoroutineScope()
+
     val animated = remember { Animatable(initialValue = 0f) }
 
-    val anim = remember {
-        TargetBasedAnimation(
-            animationSpec = tween(200),
-            typeConverter = Float.VectorConverter,
-            initialValue = 200f,
-            targetValue = 1000f
-        )
+    var isPlaying by remember { mutableStateOf(true) }
+
+    var elapsed by remember {
+        mutableIntStateOf(1000)
     }
 
 
+    Row {
+        Button(onClick = {
+            isPlaying = false
+        }) {}
+        Button(onClick = {
+            isPlaying = true
+        }) {}
+    }
 
-    LaunchedEffect(anim){
 
+    LaunchedEffect(isPlaying) {
+        scope.launch {
+            while (elapsed < time && isPlaying) {
+                elapsed += 1000
+                progress = (elapsed.toFloat() / time)
+                delay(16)
+            }
+        }
     }
 
 
@@ -90,14 +109,14 @@ fun PlayerStripe() {
             color = Color(0xFF982377),
             radius = 30.dp.toPx() / 4,
             center = Offset(
-                offsetX.coerceIn(0f, size.width),
+                (size.width * progress + offsetX).coerceIn(0f, size.width),
                 2.dp.toPx() + (4.dp.toPx() - 30.dp.toPx()) / 512
             )
         )
 
         drawRect(
             color = Color(0xFF982377),
-            size = Size(offsetX.coerceIn(0f, size.width), 4.dp.toPx()),
+            size = Size((size.width * progress + offsetX).coerceIn(0f, size.width), 4.dp.toPx()),
         )
 
 
