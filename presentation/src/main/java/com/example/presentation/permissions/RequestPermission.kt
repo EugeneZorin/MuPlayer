7770c86e7006fb22636c0afd48d1de13d848cc8c
Zubenzitkar <zubenzitkar@gmail.com>
2023-10-25 19:44:23 +0300

    0.04.7

--- presentation/src/main/java/com/example/presentation/permissions/RequestPermission.kt
+++ presentation/src/main/java/com/example/presentation/permissions/RequestPermission.kt
@@ -1,40 +1,25 @@
 package com.example.presentation.permissions
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.provider.Settings
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import com.example.presentation.navigation.MainScreens
 import com.example.presentation.viewmodels.MainViewModel
@@ -73,21 +58,29 @@ fun RequestPermission(
     dialog.reversed().forEach {
         AlertDialog(
             onDismissRequest = mainViewModel::dismissDialog,
-            title = { Text("Permission required") },
+            title = {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        textAlign = TextAlign.Center,
+                        text = "Permission required",
+                    )
+                }
+            },
             confirmButton = {
-                Divider()
                 Text(
                     text = if (!shouldShowRequestPermissionRationale(context, it)) {
                         "Grant permission"
                     } else {
                         "OK"
                     },
-                    fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Center,
                     modifier = Modifier
                         .fillMaxWidth()
                         .clickable {
-                            if (shouldShowRequestPermissionRationale(context, it)) {
+                            if (!shouldShowRequestPermissionRationale(context, it)) {
                                 context.openAppSetting()
                             } else {
                                 launcher.launch(requestPermission)
@@ -96,9 +89,23 @@ fun RequestPermission(
                         .padding(16.dp)
                 )
             },
+            text = {
+                Text(
+                    text = if(!shouldShowRequestPermissionRationale(context, it)){
+                        "Applications need permissions to start searching for audio files on the device"
+                    } else {
+                        "Applications need permissions to start searching for audio files on the device"
+                    },
+                    textAlign = TextAlign.Center
+                )
+            }
         )
     }
 }
+
+
+
+
 
 fun Activity.openAppSetting() {
     Intent(
@@ -106,6 +113,9 @@ fun Activity.openAppSetting() {
         Uri.fromParts("package", packageName, null)
     ).also(::startActivity)
 }
+
+
+
 
 
 
