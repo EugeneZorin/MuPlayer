b9c7805ef95dc515289c7b0b25335cacbe69748a
ZubenZitkar <ZubenZitkar@gmail.com>
2024-01-17 19:36:45 +0300

    0.07.4

--- presentation/src/main/java/com/example/presentation/navigation/panel/BottomPanel.kt
+++ presentation/src/main/java/com/example/presentation/navigation/panel/BottomPanel.kt
@@ -1,19 +1,27 @@
 package com.example.presentation.navigation.panel
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -64,6 +72,49 @@ fun BottomPanel() {
 }
 
 @Composable
+fun MusicInteractionPanel(
+    modifier: Modifier = Modifier
+) {
+
+    BottomAppBar(
+        modifier = modifier
+            .fillMaxHeight(0.07f),
+        containerColor = Color(0xFFF2EDF7)
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxSize(1f),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+
+
+        ) {
+            IconBottomPanel(
+                icon = painterResource(id = R.drawable.queue_music),
+                contentDescription = "Music",
+                text = "Music",
+                alignment = Alignment.Start
+            )
+            IconBottomPanel(
+                icon = painterResource(id = R.drawable.queue_music),
+                contentDescription = "Music",
+                text = "Music",
+                alignment = Alignment.Start
+            )
+            IconBottomPanel(
+                icon = painterResource(id = R.drawable.queue_music),
+                contentDescription = "Music",
+                text = "Music",
+                alignment = Alignment.Start
+            )
+
+        }
+    }
+
+
+}
+
+@Composable
 fun IconBottomPanel(
     icon: Painter,
     contentDescription: String,
@@ -71,12 +122,18 @@ fun IconBottomPanel(
     alignment: Alignment.Horizontal,
 ) {
     Column(
-        modifier = Modifier.clickable {
+        modifier = Modifier
+            .clickable(
+                enabled = false,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(bounded = true),
+            ) {
 
-        },
+            },
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+
         Icon(
             painter = icon,
             contentDescription = contentDescription,
--- presentation/src/main/java/com/example/presentation/screen/MainScreen.kt
+++ presentation/src/main/java/com/example/presentation/screen/MainScreen.kt
@@ -2,25 +2,22 @@ package com.example.presentation.screen
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.os.Build
-import android.util.Log
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -28,23 +25,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.presentation.R
+import com.example.presentation.navigation.panel.BottomPanel
+import com.example.presentation.navigation.panel.IconBottomPanel
+import com.example.presentation.navigation.panel.MusicInteractionPanel
 import com.example.presentation.screen.components.SearchView
 import com.example.presentation.screen.components.palylist.Player
-import com.example.presentation.navigation.panel.BottomPanel
 import com.example.presentation.service.PlayerService
 import com.example.presentation.viewmodels.MainViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -67,8 +64,6 @@ fun MainScreen(
     val size = quantitiesMusic.value!!.size
     val position = mainViewModel.getData.observeAsState()
     val context = LocalContext.current
-
-
 
 
     val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
@@ -122,7 +117,7 @@ fun MainScreen(
                         }
 
                     true ->
-                        Text(text = "123")
+                        MusicInteractionPanel()
 
                 }
 
@@ -184,13 +179,9 @@ fun MainScreen(
                                         isCheckedMain = when (isCheckedMain) {
                                             true -> false
                                             false -> true
-
-                                            else -> {
-                                                TODO()
-                                            }
                                         }
                                         chooseMusic(
-                                            isChecked = isCheckedMain!!,
+                                            isChecked = isCheckedMain,
                                             mainViewModel = mainViewModel,
                                             music = music
                                         )
@@ -264,15 +255,15 @@ fun MainScreen(
 }
 
 
-
 fun chooseMusic(
     isChecked: Boolean,
     music: Int,
     mainViewModel: MainViewModel
-){
+) {
     when (isChecked) {
         true ->
             mainViewModel.arrayChosenMusic.add(music)
+
         false ->
             mainViewModel.arrayChosenMusic.remove(music)
     }
